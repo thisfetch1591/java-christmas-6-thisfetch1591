493064b8444d605eaa69073719c7fc6ce432f047
thisfetch1591 <thisfetch1591@gmail.com>
2023-11-11 22:17:00 +0900

    feat: 메뉴 ENUM 개발
    - 메뉴를 입력받기 전 주어진 요구사항에 맞춰 메뉴 enum 개발

---
+++ src/main/java/constants/Menu.java
@@ -0,0 +1,36 @@
+package constants;
+
+public enum Menu {
+    MUSHROOM_CREAM_SOUP("양송이수프", 6000, 1),
+    TAPAS("타파스", 5500, 1),
+    CAESAR_SALAD("시저샐러드", 8000, 1),
+    T_BONE_STEAK("티본스테이크", 55000, 2),
+    BARBECUE_LIP("바베큐립", 54000, 2),
+    SEAFOOD_PASTA("해산물파스타", 35000, 2),
+    XMAS_PASTA("크리스마스파스타", 25000, 2),
+    CHOCOLATE_CAKE("초코케이크", 15000, 3),
+    ICE_CREAM("아이스크림", 5000, 3),
+    ZERO_COKE("제로콜라", 3000, 4),
+    RED_WINE("레드와인", 60000, 4),
+    CHAMPAGNE("샴페인", 25000, 4);
+    private String nameOfMenu;
+    private int costOfMenu;
+    private int codeOfMenu;
+
+    private Menu(String nameOfMenu, int costOfMenu, int codeOfMenu) {
+        this.nameOfMenu = nameOfMenu;
+        this.costOfMenu = costOfMenu;
+        this.codeOfMenu = codeOfMenu;
+    }
+
+    public int addPrice(int totalPrice) {
+        return totalPrice += costOfMenu;
+    }
+
+    public boolean isDrink() {
+        if (codeOfMenu == 4) {
+            return true;
+        }
+        return false;
+    }
+}
--- src/test/java/christmas/validator/InputDateValidatorTest.java
+++ src/test/java/christmas/validator/InputDateValidatorTest.java
@@ -5,29 +5,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 class InputDateValidatorTest {
 
     @DisplayName("Integer로 변환하지 못할 시 에러처리")
-    @Test
-    void createExceptionForNotInteger() {
-        String input = "삼십일일";
+    @ValueSource(strings = {"삼십일일"})
+    @ParameterizedTest
+    void createExceptionForNotInteger(String input) {
         assertThatThrownBy(() -> InputDateValidator.validateDate(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
 
-    @DisplayName("32를 입력 시 예외처리")
-    @Test
-    void createExceptionForNotExistOverDate() {
-        String input = "32";
-        assertThatThrownBy(() -> InputDateValidator.validateDate(input))
-                .isInstanceOf(IllegalArgumentException.class);
-    }
-
-    @DisplayName("0을 입력 시 예외처리")
-    @Test
-    void createExceptionForNotExistUnderDate() {
-        String input = "0";
+    @DisplayName("1~31에 해당하지 않은 숫자를 입력 시 예외처리")
+    @ValueSource(strings = {"32", "0", "-25"})
+    @ParameterizedTest
+    void createExceptionForNotExistOverDate(String input) {
         assertThatThrownBy(() -> InputDateValidator.validateDate(input))
                 .isInstanceOf(IllegalArgumentException.class);
     }
